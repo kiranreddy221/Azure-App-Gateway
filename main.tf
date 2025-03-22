02ed4a295b054f6f7327081803a196097316c0cd
Katikareddy Kiranreddy (International Supplier) <v-kakiranred@microsoft.com>
2025-03-22 14:47:22 +0530

    updated

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "appgatewayrg" {
   name = "appgatewayrg"
   location = "eastus"
 }
+resource "azurerm_webapp" "webapp" {
+    name = webapp
+    location = westus
+}
+
+resource "azurerm_appserviceplan" "serviceplan" {
+
+    name = azurerm_appserviceplan
+    sku = standarad
+}
